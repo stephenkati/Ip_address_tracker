9f4f32d0c24e18551396e11663c663738f7e8023
stephenkati <stephenkatuli88@gmail.com>
2023-10-24 15:58:51 +0300

    Create Tracker page layout

---
+++ src/pages/Tracker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header';
+
+const Tracker = () => {
+  return (
+    <div className='tracker'>
+      <Header />
+    </div>
+  )
+}
+
+export default Tracker;
