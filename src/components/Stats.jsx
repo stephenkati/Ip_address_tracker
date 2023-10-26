7c8989d34c1362c09a33aa9a3782edc14888f120
stephenkati <stephenkatuli88@gmail.com>
2023-10-26 10:01:42 +0300

    Display location stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 
-const Stats = () => {
+const Stats = ({
+  ipAddress,
+  location,
+  timezone,
+  isp
+}) => {
   return (
     <div className='stats-sec'>
       <div className='stats'>
         <div className='stats-item'>
           <p>IP ADDRESS</p>
-          <span>192.168.0.100</span>
+          <span> {ipAddress} </span>
         </div>
 
         <div className='stats-item'>
           <p>LOCATION</p>
-          <span>Brooklyn, NY 10001</span>
+          <span> {location} </span>
         </div>
 
         <div className='stats-item'>
           <p>TIMEZONE</p>
-          <span>UTC -05:00</span>
+          <span> {timezone} </span>
         </div>
 
         <div className='stats-item'>
           <p>ISP</p>
-          <span>SpaceX Starlink</span>
+          <span> {isp} </span>
         </div>
       </div>
     </div>
